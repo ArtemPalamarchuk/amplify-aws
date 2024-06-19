78e62eaa57c63799788a374dfb5da652282a8fd2
artemandreevich <foks992@gmail.com>
2024-06-19 20:35:18 +0300

    clean

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,5 @@
 <script lang="ts">
 import { defineComponent } from "vue";
-import { useRemindersStore } from "@/store/reminders";
-import { useListsStore } from "@/store/lists";
 
 export default defineComponent({
 
